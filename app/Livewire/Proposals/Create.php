e19b5d72b92adf240c731f9cbe67843573eba53b
Guilherme Santos <guilhermedesousa.dev@gmail.com>
2024-10-12 18:24:44 -0300

    add notifications

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Proposals;
 use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
+use App\Notifications\DroppedInRanking;
+use App\Notifications\NewProposal;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
@@ -37,6 +39,7 @@ class Create extends Component
             $this->arrangePositions($proposal);
         });
 
+        $this->project->author->notify(new NewProposal($this->project));
 
         $this->dispatch('proposal::created');
         $this->modal = false;
@@ -60,7 +63,9 @@ class Create extends Component
 
         if ($otherProposal) {
             $proposal->update(['position_status' => 'up']);
-            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+            $oProposal = Proposal::find($otherProposal->id);
+            $oProposal->update(['position_status' => 'down']);
+            $oProposal->notify(new DroppedInRanking($this->project));
         }
 
         ArrangePositions::run($proposal->project_id);
