451b8fbe767cc9b196b469cbe844c84856a37f36
Guilherme Santos <guilhermedesousa.dev@gmail.com>
2024-10-12 18:27:19 -0300

    add drop in ranking notification to queue

--- app/Notifications/DroppedInRanking.php
+++ app/Notifications/DroppedInRanking.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class DroppedInRanking extends Notification
+class DroppedInRanking extends Notification implements ShouldQueue
 {
     use Queueable;
 
